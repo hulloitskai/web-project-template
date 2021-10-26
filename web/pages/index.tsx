17f302fb6bb26c7ee83f3c5feaf47d75a3cb5eba
Kai <hulloitskai@gmail.com>
2021-10-25 23:33:03 -0400

    ✨ (web) Show example data fetch from api

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useMemo } from "react";
 import type { NextPage } from "next";
 
+import { gql } from "@apollo/client";
+import { useHomePageQuery } from "apollo";
+
+import { VStack } from "@chakra-ui/react";
+import { Skeleton } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
+import { chakra } from "@chakra-ui/react";
+
+gql`
+  query HomePage {
+    buildInfo {
+      timestamp
+      version
+    }
+  }
+`;
 
 const HomePage: NextPage = () => {
+  const { data } = useHomePageQuery();
+  const dataText = useMemo(() => JSON.stringify(data, undefined, 2), [data]);
   return (
-    <Text fontSize="3xl" fontWeight="bold">
-      Hello World!
-    </Text>
+    <VStack spacing={4} my={24}>
+      <Text fontSize="3xl" fontWeight="extrabold">
+        HELLO WORLD
+      </Text>
+      <Skeleton isLoaded={!!data} minH={32}>
+        <chakra.pre>{dataText}</chakra.pre>
+      </Skeleton>
+    </VStack>
   );
 };
 
